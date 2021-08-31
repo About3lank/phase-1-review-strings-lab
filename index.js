e38dd52bb36179f2183596dc548d756827fd1760
Matthew Blank <matthew.3lank@gmail.com>
2021-08-30 22:17:01 -0400

    commit tree 619a8e1b21c70a72582cd27994089dc1d023cc2c

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const currentUser = 'Timmy Turner';
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser[0]}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
